2fe5d542e064bac7e6440184eaf231d4fe697519
breandan <bre@ndan.co>
2020-11-29 11:09:34 -0500

    update and remove unneeded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,29 +2,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   application
-  kotlin("jvm") version "1.4.10"
+  kotlin("jvm") version "1.4.20"
 }
 
 group = "me.breandan"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    jcenter()
-    maven("https://jitpack.io")
-    maven("https://dl.bintray.com/mipt-npm/dev")
-    maven("https://dl.bintray.com/hotkeytlt/maven")
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven("https://dl.bintray.com/egor-bogomolov/astminer")
-    maven("https://maven.jzy3d.org/releases")
-    maven("https://jetbrains.bintray.com/lets-plot-maven")
-    maven("http://logicrunch.research.it.uu.se/maven/")
-    maven("https://clojars.org/repo")
+  jcenter()
+  maven("https://jitpack.io")
+  maven("https://dl.bintray.com/egor-bogomolov/astminer")
 }
 
 dependencies {
   testImplementation(kotlin("test-junit"))
-  implementation("com.github.breandan:kotlingrad:0.3.7")
-  implementation("com.github.breandan:kaliningraph:0.1.2")
+  implementation("com.github.breandan:kotlingrad:0.3.9")
+  implementation("com.github.breandan:kaliningraph:0.1.3")
 }
 
 application {
