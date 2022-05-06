6d7a1784fd919d1d2fcffae32dbb58060d65e7aa
breandan <bre@ndan.co>
2022-05-05 23:32:08 -0400

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,26 +2,22 @@ import org.gradle.api.JavaVersion.VERSION_15
 
 plugins {
     application
-    kotlin("jvm") version "1.6.20-dev-1357"
+    kotlin("jvm") version "1.6.21"
 }
 
 group = "me.breandan"
 version = "1.0-SNAPSHOT"
 
-java.toolchain {
-  languageVersion.set(JavaLanguageVersion.of(15))
-  vendor.set(JvmVendorSpec.ADOPTOPENJDK)
-  implementation.set(JvmImplementation.J9)
+kotlin.jvmToolchain {
+    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(15))
 }
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 dependencies {
-    implementation("ai.hypergraph:kotlingrad:0.4.6")
+    implementation("ai.hypergraph:kotlingrad:0.4.7")
 }
 
 application {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,4 @@ rootProject.name = "kotlingrad_consumer"
 pluginManagement.repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
